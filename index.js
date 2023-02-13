df839e5909ba18a4a4a5926cc1cdb65ce6282ad1
fetraniain <tahiri.fetra@gmail.com>
2023-02-13 17:46:23 +0100

    first two commit

--- index.js
+++ index.js
@@ -4,27 +4,14 @@ const bodyParser = require('body-parser')
 const app = express()
 const mysql = require('mysql2');
 
+app.set('views',path.join(__dirname,'templates'))
+app.set('view engine', 'twig')
 
+//middleware
+app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 app.use(bodyParser.urlencoded({extends:true}))
 app.use(bodyParser.json())
-
-
-app.get('/',(req,res)=>{
-      connection.query(
-  'SELECT * FROM `users`',
-  function(err, results) {
-    if(results){
-       res.send(results)
-    }
-  }
-)
-})
-
-
-
-
-
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -36,6 +23,48 @@ const connection = mysql.createConnection({
 
 
 
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/service',(req,res)=>{
+      connection.query(
+  'SELECT * FROM `users`',
+  function(err, results) {
+    if(results){
+        console.log(results)
+         res.render('service',{data:results})
+    }
+  }
+)
+})
+
+app.get('/chat',(req,res)=>{
+
+  res.render('register')
+   
+})
+
+app.post('/postUser',(req,res)=>{
+     connection.query(
+  'INSERT INTO users(user,email,password) VALUES(?,?,?)',[req.body.user,req.body.email,req.body.password],
+  function(err, results) {
+    if(results){
+         res.redirect('/chat')
+         console.log('data inserted')
+    }else{
+        res.redirect('/chat')
+        console.log(err)
+    }
+  }
+)
+})
+
+
+app.get('/team',(req,res)=>{
+    res.render('teams')
+})
 
 
 
